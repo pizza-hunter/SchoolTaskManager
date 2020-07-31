b5bc292afe72d2560da5076da8385c967472db78
HunterGreen <hunter.green95@hotmail.com>
2020-07-31 16:15:22 +0930

    Commented list of Tests to create, to apply TDD

--- SchoolTaskManagerTests/SchoolTaskManagerTests.swift
+++ SchoolTaskManagerTests/SchoolTaskManagerTests.swift
@@ -10,6 +10,36 @@ import XCTest
 @testable import SchoolTaskManager
 
 class SchoolTaskManagerTests: XCTestCase {
+    
+    /*
+    Trialing Test driven development
+    TEST REQUIREMENTS
+     
+     SYSTEM TESTS
+     Create a 'task'
+     included: name, due date, weight of grade, notes
+     Store a task
+     Edit a task
+     Delete a task
+     Sort task alphabetically
+     Sort task chronologically (creation)
+     Sort task chronologically (due date)
+     Sort task weight (grade)
+     Create a subject
+     Store a task within a subject
+     Categorise a task as long term (assignment) or short term (reading)
+     Store subtasks within a task
+     
+     
+     UI TESTS
+     View tasks per subject
+     View tasks for all subjects
+ 
+     MAY BE TOO TIME CONSUMING
+     Set a task to repeat weekly
+     
+     
+     */
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
