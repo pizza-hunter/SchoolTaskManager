51f6590f1c3bc4e0343d2f9551be4e46bec76cb7
HunterGreen <hunter.green95@hotmail.com>
2020-07-31 15:43:55 +0930

    Initial Commit

---
+++ SchoolTaskManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SchoolTaskManager
+//
+//  Created by Hunter Green on 31/07/2020.
+//  Copyright © 2020 Hunter Green. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SchoolTaskManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SchoolTaskManager
+//
+//  Created by Hunter Green on 31/07/2020.
+//  Copyright © 2020 Hunter Green. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
